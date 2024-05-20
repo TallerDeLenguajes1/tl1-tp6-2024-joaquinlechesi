0762c2ab56fe321618d7c6bef6ff39be552a6d5c
joaquinlechesi <joaquinlechesi@gmail.com>
2024-05-20 15:03:32 -0300

    Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -4,17 +4,31 @@ using System.Security;
 Console.WriteLine("Bienvenido a la CalculadoraV1");
 
 string opcion;
-opcion = Console.ReadLine();
+//opcion = Console.ReadLine();
 int opcionNumerica = 0;
-int.TryParse(opcion, out opcionNumerica);
+
 
 do
 {
     Console.WriteLine("Seleccione un operacion de la calculadora:\n1 para la operacion SUMA\n2 para la operacion RESTA\n3 para la operacion MULTIPLICACION\n4 para la operacion DIVISION");
+    opcion = Console.ReadLine();
+    int.TryParse(opcion, out opcionNumerica);
     switch (opcionNumerica)
     {
-        
-        default:
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
             break;
     }
-} while (opcionNumerica < 0 || opcionNumerica > 4);
+    Console.WriteLine("Desea realizar otra operacion? Ingrese 0 para NO, 1 para SI");
+    opcion = Console.ReadLine();
+    int.TryParse(opcion, out opcionNumerica);
+} while (opcionNumerica != 0);
