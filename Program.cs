5cf24e7c32d3061135cb97ed3457d0dc0829cfa6
joaquinlechesi <joaquinlechesi@gmail.com>
2024-05-21 21:10:39 -0300

    Ejercicio 2

--- Program.cs
+++ Program.cs
@@ -13,19 +13,59 @@ do
     Console.WriteLine("Seleccione un operacion de la calculadora:\n1 para la operacion SUMA\n2 para la operacion RESTA\n3 para la operacion MULTIPLICACION\n4 para la operacion DIVISION");
     opcion = Console.ReadLine();
     int.TryParse(opcion, out opcionNumerica);
+    string numero;
+    int numero1 = 0, numero2 = 0, resultado = 0;
     switch (opcionNumerica)
     {
         case 1:
-
+            Console.WriteLine("Ingrese el primer numero:");
+            numero = Console.ReadLine();
+            int.TryParse(numero, out numero1);
+            Console.WriteLine("Ingrese el segundo numero:");
+            numero = Console.ReadLine();
+            int.TryParse(numero, out numero2);
+            resultado = numero1 + numero2;
+            Console.WriteLine("El resultado es: " + resultado);
             break;
         case 2:
-
+            Console.WriteLine("Ingrese el primer numero:");
+            numero = Console.ReadLine();
+            int.TryParse(numero, out numero1);
+            Console.WriteLine("Ingrese el segundo numero:");
+            numero = Console.ReadLine();
+            int.TryParse(numero, out numero2);
+            if (numero1 < numero2)
+            {
+                Console.WriteLine("El primer numero es menor que el segundo, no es posible realizar la operacion RESTA");
+            }else
+            {
+                resultado = numero1 - numero2;
+                Console.WriteLine("El resultado es: " + resultado);
+            }
             break;
         case 3:
-
+            Console.WriteLine("Ingrese el primer numero:");
+            numero = Console.ReadLine();
+            int.TryParse(numero, out numero1);
+            Console.WriteLine("Ingrese el segundo numero:");
+            numero = Console.ReadLine();
+            int.TryParse(numero, out numero2);            
             break;
         case 4:
-
+            Console.WriteLine("Ingrese el primer numero:");
+            numero = Console.ReadLine();
+            int.TryParse(numero, out numero1);
+            Console.WriteLine("Ingrese el segundo numero:");
+            numero = Console.ReadLine();
+            int.TryParse(numero, out numero2);
+            if (numero1 < numero2)
+            {
+                Console.WriteLine("El primer numero es menor que el segundo numero, no es posible realizar la operacion DIVICION.");
+            }else
+            {
+                resultado = numero1 / numero2;
+                Console.WriteLine("El resultado es: " + resultado);
+            }
             break;
     }
     Console.WriteLine("Desea realizar otra operacion? Ingrese 0 para NO, 1 para SI");
