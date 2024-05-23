6d391c87dca18ba89f6359d0208ce005aeaded3c
Joaquin <joaquinlechesi@gmail.com>
2024-05-23 16:43:22 -0300

    Ejercicio 3

--- Program.cs
+++ Program.cs
@@ -1,3 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Binvenido a la CalculadoraV2");
 
+string opcion;
+int opcionInt;
+opcion = Console.ReadLine();
+int.TryParse(opcion, out opcionInt);
+
+do
+{
+    Console.WriteLine("Ingrese un numero para seleccionar una opcion\n1 para la operacion VALOR ABSOLUTO\n2 para la operacion CUADRADO\n3 para la operacion RAIZ CUADRADA\n4 para la operacion SENO\n5 para la operacion COSENO\n6 para la operacion PARTE ENTERA DE UN FLOAT");
+    switch (opcionInt)
+    {
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        default:
+            break;
+    }
+} while (opcionInt != 0);
